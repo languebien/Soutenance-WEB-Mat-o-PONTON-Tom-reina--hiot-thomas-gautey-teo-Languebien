5bb4b48b10b30304b8e7cba00359d6c1253f7d85
Royal69Pizza <you@example.com>
2022-02-28 10:49:10 +0100

    Dossier Modèle à Modifier

---
+++ Modele/bd.utilisateur.inc.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+
+Modifier les requetes SQL et les arguments des fonctions.
+
+*/
+
+include_once "bd.inc.php";
+
+function getUtilisateurs() {
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("SELECT * FROM fightclub");
+        $req->execute();
+
+        $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        while ($ligne) {
+            $resultat[] = $ligne;
+            $ligne = $req->fetch(PDO::FETCH_ASSOC);
+        }
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function getUtilisateursByPseudo($pseudo) {
+
+    try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("SELECT * FROM fightclub WHERE pseudo = :pseudo");
+        $req->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $req->execute();
+
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+        
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function addUtilisateur($pseudo, $optionbts, $pizzapass) {
+    try {
+        $cnx = connexionPDO();
+
+        $mdpUCrypt = crypt($pizzapass, "sel");
+        $req = $cnx->prepare("insert into fightclub (pseudo, optionbts, pizzapass) values(:pseudo,:optionbts,:mdp)");
+        $req->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $req->bindValue(':optionbts', $optionbts, PDO::PARAM_STR);
+        $req->bindValue(':mdp', $mdpUCrypt, PDO::PARAM_STR);
+        
+        $resultat = $req->execute();
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+?>
---
+++ getRacine.php
@@ -0,0 +1,7 @@
+<?php
+
+/* R A C I N E */
+
+$racine = dirname(__FILE__);
+
+?>
