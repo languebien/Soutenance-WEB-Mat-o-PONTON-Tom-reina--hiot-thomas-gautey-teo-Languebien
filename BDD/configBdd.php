1cf3adf12df06b8b09d76ee8dff04173ba6bf095
teo <teo.languebien@clsi.fr>
2022-02-28 11:42:43 +0100

    bdd

--- BDD/configBdd.php
+++ BDD/configBdd.php
@@ -1,7 +1,7 @@
 <?php
 $PARAM_hote='localhost'; // le chemin vers le serveur
 $PARAM_port='3306';
-$PARAM_nom_bd='crud-exemple'; // le nom de votre base de données
+$PARAM_nom_bd='Soutenance-WEB'; // le nom de votre base de données
 $PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
-$PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
+$PARAM_mot_passe='root'; // mot de passe de l'utilisateur pour se connecter
 ?>
