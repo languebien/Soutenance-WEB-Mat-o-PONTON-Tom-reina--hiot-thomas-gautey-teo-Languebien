22cbed4021e3f6a58655964787993f5abbfcef5f
Royal69Pizza <you@example.com>
2022-03-05 19:06:46 +0100

    Update le 05/03/22

--- controleur/controleurAcceuil.php
+++ controleur/controleurAcceuil.php
@@ -3,6 +3,10 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
 
+$title = "Compagnie Océane - Accueil";
+$keywords = "accueil compagnie Océane";
+$description = "page d'accueil de la Compagnie Océane";
+
 include "$racine/vue/entete.php";
 include "$racine/vue/menu.php";
 include "$racine/vue/acceuil.php";
