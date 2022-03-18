e89302f951bc0ab2f93140ba31b7d2b1403c8510
teo <teo.languebien@clsi.fr>
2022-03-18 16:08:54 +0100

    git

--- Modele/bd.inc.php
+++ Modele/bd.inc.php
@@ -3,11 +3,8 @@
 
 
 function connexionPDO() {
-    $login = "root";
-    $mdp = "root";
-    $bd = "oceane";
-    $serveur = "localhost";
-
+    
+    include ('Modele/LocalBd.inc.php');
     try {
         $conn = new PDO("mysql:host=$serveur;dbname=$bd", $login, $mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')); 
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
