68b6db69c649b1c1d87d045d214dcc94358a9b7d
tita <teo.languebien@clsi.fr>
2022-03-19 15:05:54 +0100

    d

--- vue/pied_page.php
+++ vue/pied_page.php
@@ -1,5 +1,5 @@
         <footer class = "Footer">
-            <p class="Footer_Text">Théo LANGUEBIEN</p>
+            <p class="Footer_Text">Téo LANGUEBIEN</p>
             <p class="Footer_Text">Mateo PONTON</p>
             <p class="Footer_Text">Thomas GAUTHEY</p>
             <p class="Footer_Text">Tom REINA--HIOT</p>
