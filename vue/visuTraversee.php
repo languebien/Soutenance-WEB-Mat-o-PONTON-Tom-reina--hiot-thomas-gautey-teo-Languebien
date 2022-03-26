e5604fa718d4d791f732d5fc1052deec2380f62c
Royal69Pizza <you@example.com>
2022-03-26 12:55:35 +0100

    Chocolate Whale

--- vue/visuTraversee.php
+++ vue/visuTraversee.php
@@ -4,16 +4,10 @@
 		<div class = "Pizza">
 			<h1 class = "Titre">Réserver une Traversée</h1>
 			<form id = "AfficherBateaux" action = "">
-				<div class = "Secteurs">
-				<?php
-					/* foreach ($Secteur as $TitreSecteur) {
-						echo 
-						"<input>".$TitreSecteur[' ']."</input>"; // Nom du secteur
-					} */
-				?>
-				</div>
 				<div class = "Filtres">
-					<p class = "Text">Séléctionnez la liaison et la date souhaitée : </p>
+					<select class = "SelectionneSecteur" name = "SelectionneSecteur" title = "SelectionneSecteur">
+						<option value="">Sélectionnez un Secteur</option>
+					</select>
 					<select class = "SelectionneLiaison" name = "SelectionneLiaison" title = "SelectionneLiaison">
 						<option value="">Sélectionnez une liaison</option>
 					</select>
@@ -25,30 +19,34 @@
 			</form>
 			<table class = "Table">
 				<thead>
-					<th class = "Table_thead_th">ID</th>
-					<th class = "Table_thead_th">Nom</th>
-					<th class = "Table_thead_th">Longueur</th>
-					<th class = "Table_thead_th">Largeur</th>
-					<th class = "Table_thead_th">Vitesse</th>
-					<th class = "Table_thead_th">Nb Passagers</th>
-					<th class = "Table_thead_th">Nb Vehicules</th>
+					<th class = "Table_thead_th" colspan="3">Traversée</th>
+					<th class = "Table_thead_th" colspan="3">Places disponibles par catégorie</th>
+				</thead>
+				<thead>
+					<th class = "Table_thead_th">N°</th>
+					<th class = "Table_thead_th">Heure</th>
+					<th class = "Table_thead_th">Bateau</th>
+					<th class = "Table_thead_th">A - Passagers</th>
+					<th class = "Table_thead_th">B - Veh. inf. 2M</th>
+					<th class = "Table_thead_th">C - Veh. sup. 2M</th>
 				</thead>
 				<tbody>
-					<?php 
-					
-					foreach ($bateaux as $row){
-						echo 
-						"<tr>
-							<td>".$row['id']."</td>
-							<td>".$row['nom']."</td>
-							<td>".$row['longueur']."</td>
-							<td>".$row['largeur']."</td>
-							<td>".$row['vitesse']."</td>
-							<td>".$row['nbPassager']."</td>
-							<td>".$row['nbVehicule']."</td>
-						</tr>";
-					}
-					?>
+					<tr>
+						<td>TEST TABLEAU</td>
+						<td>12:41</td>
+						<td>Flying Pizza</td>
+						<td>582</td>
+						<td>123</td>
+						<td>41</td>
+					</tr>
+					<tr>
+						<td>TEST TABLEAU 2</td>
+						<td>12:53</td>
+						<td>Vanilla Raptor</td>
+						<td>485</td>
+						<td>32</td>
+						<td>5</td>
+					</tr>
 				</tbody>
 			</table>
 		</div>
