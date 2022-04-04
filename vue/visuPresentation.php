5b0c09534b9f7c0e1dabd241b60b783ee5591f23
Matpon11 <mateo.ponton@clsi.Fr>
2022-04-04 12:11:23 +0200

    okok

--- vue/visuPresentation.php
+++ vue/visuPresentation.php
@@ -1,6 +1,11 @@
 ﻿        <div class = "Pizza">
-            <p class = "Titre">Acceuil</p>
-            <p class = "Text">Fat raccoon eat peanuts ! Bienvenue sur la page d'acceuil de la compagnie océane !</p>
+            <p class = "Titre">HOME</p>
+            <img src="./images/acceuil.png" id="pixelperfect">
+            </img>
+            <p class = "Description">Comming soon</br>
+            </br>
+            </br>
+            </p>
         </div>
         <video src="./video/video.mp4" id="videoacceuil" autoplay loop muted>
         </video>
