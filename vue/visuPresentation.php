d50f4c60f2073968223a1222c5c29571cedad3fc
Royal69Pizza <you@example.com>
2022-04-21 13:21:16 +0200

    ultra creamy raccoon

--- vue/visuPresentation.php
+++ vue/visuPresentation.php
@@ -1,15 +1,18 @@
-﻿
-       
-        <div class = "Pizza">
+﻿        <div class = "Pizza">
             <p class = "Titre">Océane</p>
 
             </br>
             </br>
+
             <p class = "petitedescription">Bonjour</p>
+
             </br>
             </br>
+
             <p class = "grossedescription"><strong>Tout commence avec un voyage</strong></p>
+
             </br>
+
             <p class = "moyennedescription">Laissez vous porter au fil des vagues</br></p>
             <p class = "moyennedescriptionz">rien de plus important que de commencer</p>
             <p class = "moyennedescriptionzz">chaque journée du bon pied</p>
@@ -17,17 +20,9 @@
 
             </br>
 
-            
             <a href="index.php?action=afficheTraversee" id="btnpresentation"><span>reservation</span>
-            <div class="wave"></div>
-
-        </a>
-
-                
+                <div class="wave"></div></a>  
         </div>
-                
-                     
         
-        <video src="./video/video.mp4" id="videoacceuil" autoplay loop muted>
-        </video>
+        <video src="./video/video.mp4" id="videoacceuil" autoplay loop muted></video>
         
