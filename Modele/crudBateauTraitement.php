2d9916889f444f354de2903bd3fd0a7f003c86c5
teo <teo.languebien@clsi.fr>
2022-05-06 15:18:52 +0200

    r

--- Modele/crudBateauTraitement.php
+++ Modele/crudBateauTraitement.php
@@ -2,30 +2,7 @@
 	include_once('Modele/bd.inc.php');
 	
 
-	if(isset($_POST['add'])){
-		$nom = $_POST['nom'];
-		$longueur = $_POST['longueur'];
-		$largeur = $_POST['largeur'];
-		$vitesse = $_POST['vitesse'];
-		$nbP = $_POST['nbP'];
-		$nbV = $_POST['nbV'];
-		$connexion = connexionPDO();
-		$req = $connexion->prepare('INSERT INTO bateau (nom, longueur, largeur, vitesse, nbPassager, nbVehicule) VALUES (:nom, :longueur, :largeur, :vitesse, :nbP, :nbV)');
-		$req->bindParam(':nom', $nom, PDO::PARAM_STR);
-		$req->bindParam(':longueur', $longueur, PDO::PARAM_INT);
-		$req->bindParam(':largeur', $largeur, PDO::PARAM_INT);
-		$req->bindParam(':vitesse', $vitesse, PDO::PARAM_INT);
-		$req->bindParam(':nbP', $nbP, PDO::PARAM_INT);
-		$req->bindParam(':nbV', $nbV, PDO::PARAM_INT);
-		$resultat = $req->execute();
-		if($resultat){
-			$_SESSION["success"] = 'Bateau ajouté';
-		}
-		else{
-			$_SESSION["error"] = 'Problème lors de l\'ajout du bateau';
-		}
-		header('location: index.php?action=modifBateau');
-	}
+	
 	
 	if(isset($_POST['edit'])){
 		$id = $_POST['id'];
