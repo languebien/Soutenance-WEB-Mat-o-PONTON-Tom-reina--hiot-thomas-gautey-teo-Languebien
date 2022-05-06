982c12b7f647c9d2c591372b0317243673d31fa5
teo <teo.languebien@clsi.fr>
2022-05-06 15:19:01 +0200

    d

--- Modele/bd.bateau.php
+++ Modele/bd.bateau.php
@@ -38,4 +38,32 @@ function addBateau($nom, $longueur, $largeur, $vitesse, $nbP, $nbV) {
     return $resultat;
 }
 
+
+function editBateau($nom, $longueur, $largeur, $vitesse, $nbP, $nbV) {
+    include_once('Modele/bd.inc.php');
+    $connexion = connexionPDO();	
+    $req = $connexion->prepare('UPDATE bateau SET nom = :nom, longueur = :longueur, largeur = :largeur, vitesse = :vitesse, nbPassager = :nbP, nbVehicule = :nbV WHERE id = :id');
+    $req->bindParam(':nom', $nom, PDO::PARAM_STR);
+    $req->bindParam(':id', $id, PDO::PARAM_INT);
+    $req->bindParam(':longueur', $longueur, PDO::PARAM_INT);
+    $req->bindParam(':largeur', $largeur, PDO::PARAM_INT);
+    $req->bindParam(':vitesse', $vitesse, PDO::PARAM_INT);
+    $req->bindParam(':nbP', $nbP, PDO::PARAM_INT);
+    $req->bindParam(':nbV', $nbV, PDO::PARAM_INT);
+    $resultat = $req->execute();
+    return $resultat;
+}
+
+
+
+function suprBateau($id) {
+    include_once('Modele/bd.inc.php');
+    $connexion = connexionPDO();
+    $req = $connexion->prepare('DELETE FROM bateau WHERE id = :id ');
+    $req->bindParam(':id', $id, PDO::PARAM_INT);
+    $resultat = $req->execute();
+}
+
+
+
 ?>
--- controleur/modifbateau.php
+++ controleur/modifbateau.php
@@ -23,6 +23,36 @@ if(isset($_POST['add'])){
     }
     header('location: index.php?action=modifBateau');
 }
+if(isset($_POST['edit'])){
+    $nom = $_POST['nom'];
+    $longueur = $_POST['longueur'];
+    $largeur = $_POST['largeur'];
+    $vitesse = $_POST['vitesse'];
+    $nbP = $_POST['nbP'];
+    $nbV = $_POST['nbV'];
+
+
+    $resultat = addBateau($nom, $longueur, $largeur, $vitesse, $nbP, $nbV);
+    if($resultat){
+        $_SESSION["success"] = 'Bateau ajouté';
+    }
+    else{
+        $_SESSION["error"] = 'Problème lors de l\'ajout du bateau';
+    }
+    header('location: index.php?action=modifBateau');
+}
+if(isset($_POST['supr'])){
+    $id = $_POST['id'];
+
+    $resultat = suprBateau($id);
+    if($resultat){
+        $_SESSION["success"] = 'Bateau ajouté';
+    }
+    else{
+        $_SESSION["error"] = 'Problème lors de l\'ajout du bateau';
+    }
+    header('location: index.php?action=modifBateau');
+}
 
 include("vue/entete.php");
 include('vue/menu.php');
