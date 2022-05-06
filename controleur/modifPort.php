00ba2e1b86781403b85d5d30f532d0af0130cf87
teo <teo.languebien@clsi.fr>
2022-05-06 15:47:32 +0200

    s

--- controleur/modifPort.php
+++ controleur/modifPort.php
@@ -5,6 +5,48 @@ include ("Modele/bd.authentification.inc.php");
 
 $Ports = getPorts();
 
+if(isset($_POST['add'])){
+    $nom = $_POST['nom'];
+    $adresse = $_POST['adresse'];
+    $code = $_POST['codePostal'];
+    $ville = $_POST['ville'];
+    $lieu = $_POST['lieu'];
+    
+    $resultat = addPort($nom,$adresse,$code,$ville,$lieu);
+    if($resultat){
+        $_SESSION["success"] = 'Port ajouté';
+    }
+    else{
+        $_SESSION["error"] = 'Problème lors de l\'ajout du Port';
+    }
+    header('location: index.php?action=modifPort');
+}
+
+if(isset($_POST['edit'])){
+    $id = $_POST['id'];
+    $nom = $_POST['nom'];	
+    
+    $resultat = editPort($id,$nom);
+    if($resultat){
+        $_SESSION['success'] = 'Port modifié';
+    }		
+    else{
+        $_SESSION['error'] = 'Problème lors de la modification du Port';
+    }
+    header('location: index.php?action=modifPort');
+}
+
+if(isset($_POST['supr'])){
+    $id = $_POST['id'];
+    $resultat =suprPort($id);
+    if($resultat){
+        $_SESSION['success'] = 'Port supprimé';
+    }		
+    else{
+        $_SESSION['error'] = 'Problème lors de la suppression du Port';
+    }
+    header('location: index.php?action=modifPort');
+}
 include("vue/entete.php");
 include('vue/menu.php');
 include("vue/visuCrudPorts.php");
