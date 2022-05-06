7e65b4be7e964e999a9fb1e674466154a398d7ba
Royal69Pizza <you@example.com>
2022-05-06 15:18:41 +0200

    cosmic megaloodon eating creamy drum & bass

--- controleur/modifPort.php
+++ controleur/modifPort.php
@@ -1,13 +1,12 @@
 <?php
 
-include ("Modele/bd.informations.inc.php");
+include ("Modele/bd.port.php");
 include ("Modele/bd.authentification.inc.php");
 
 $Ports = getPorts();
 
 include("vue/entete.php");
 include('vue/menu.php');
-include_once ("Modele/crudPortTraitement.php");
 include("vue/visuCrudPorts.php");
 include("vue/pied_page.php");
 
--- controleur/modifUtilisateurs.php
+++ controleur/modifUtilisateurs.php
@@ -1,13 +1,12 @@
 <?php
 
-include ("Modele/bd.informations.inc.php");
 include ("Modele/bd.authentification.inc.php");
 include_once('Modele/bd.utilisateur.inc.php');
+
 $Utilisateurs = getUtilisateurs();
 
 include("vue/entete.php");
 include('vue/menu.php');
-include_once ("Modele/crudUtilisateursTraitement.php");
 include("vue/visuCrudUtilisateurs.php");
 include("vue/pied_page.php");
 
